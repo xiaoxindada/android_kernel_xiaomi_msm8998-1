f2c840783dc78b05fd4f3a8db5e4fbb4037d44bf
xiaoxindada <2245062854@qq.com>
2022-07-27 20:46:23 +0800

    make.sh: Improve the code and add GCC build support
    Signed-off-by: xiaoxindada <2245062854@qq.com>

--- make.sh
+++ make.sh
@@ -7,13 +7,14 @@ tmpdir="$LOCALDIR/tmp"
 anykernel_name="AnyKernel3"
 defconfig="sagit_defconfig"
 
-case "$1" in
-"-c" | "--clean")
-    rm -rf out
-    ;;
-esac
+usage() {
+    cat <<EOF
+    $0 [-c|--clean] <--clone_toolchains|--with_clang|--with_gcc>
+EOF
+    exit 1
+}
 
-rm -rf build.log
+rm -rf $LOCALDIR/build.log
 
 clone_clang() {
     rm -rf $LOCALDIR/clang
@@ -31,6 +32,7 @@ build_with_clang() {
     local args="O=out \
           ARCH=arm64 \
           SUBARCH=arm64 \
+          SRCARCH=arm64 \
           CC=clang \
           CLANG_TRIPLE=aarch64-linux-gnu- \
           CROSS_COMPILE=aarch64-linux-android- \
@@ -38,6 +40,32 @@ build_with_clang() {
           CROSS_COMPILE_COMPAT=arm-linux-androideabi-"
 
     export PATH="${LOCALDIR}/clang/bin:${LOCALDIR}/gcc4.9/bin:${LOCALDIR}/gcc4.9_32/bin:${PATH}" # clang
+    START_TIME=$(date +%s)
+    make $args $defconfig
+    make mrproper
+    make -j12 $args
+    if [ $? = 0 ]; then
+        echo -e "\033[32m [INFO] Build successfully \033[0m"
+        END_TIME=$(date +%s)
+        EXEC_TIME=$((${END_TIME} - ${START_TIME}))
+        EXEC_TIME=$((${EXEC_TIME} / 60))
+        echo "Runtime is: ${EXEC_TIME} min"
+    else
+        echo -e "\033[31m [ERROR] Build filed \033[0m"
+        exit 1
+    fi
+}
+
+build_with_gcc() {
+    local args="O=out \
+          ARCH=arm64 \
+          SUBARCH=arm64 \
+          SRCARCH=arm64
+          CROSS_COMPILE=aarch64-linux-gnu- \
+          CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
+          CROSS_COMPILE_COMPAT=arm-linux-gnueabi-"
+
+    # export PATH="${LOCALDIR}/gcc4.9/bin:${LOCALDIR}/gcc4.9_32/bin:${PATH}" # gcc
     START_TIME=$(date +%s)
     make $args $defconfig
     make mrproper
@@ -72,10 +100,28 @@ pack_kernel() {
     cd $LOCALDIR
     rm -rf $tmpdir
     [ -f $LOCALDIR/$pack_archive ] && echo "output: $LOCALDIR/$pack_archive"
-
 }
 
-#clone_clang
-#clone_gcc
-build_with_clang
-pack_kernel
+case "$1" in
+"-c" | "--clean")
+    rm -rf $LOCALDIR/out
+    shift
+    ;;
+"-h"| "--help")
+    usage
+    shift
+    ;;
+esac
+
+if echo $@ | grep -qo -- "^--clone_toolchains$"; then
+    clone_gcc
+    clone_clang
+elif echo $@ | grep -qo -- "^--with_clang$"; then
+    build_with_clang
+    pack_kernel
+elif echo $@ | grep -qo -- "^--with_gcc$"; then
+    build_with_gcc
+    pack_kernel
+else
+    usage
+fi
